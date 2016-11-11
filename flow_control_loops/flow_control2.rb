20ecc1cf73646dc54425a62cdeba1a1c43b405d2
sergener <sibel.ergener@gmail.com>
2016-11-11 18:25:47 -0500

    Alkis work

--- flow_control_loops/flow_control2.rb
+++ flow_control_loops/flow_control2.rb
@@ -1,9 +1,18 @@
 puts "Number please!"
-#gets.chomp
+number=Integer(gets.chomp)
 
-if #what are we comparing?
-	#what can we print out?
-#end
+if number  >10 && number <100
+ 	puts "you picked a 2 digit number"
+ elsif number <0
+ 	puts "you picked a negative number"
+ elsif number >=100 && number <1000
+ 	puts "you picked a triple digit number"
+elsif number >=1000
+	puts "that is a large number"
+elsif number >=1 && number <10
+	puts "make it larger!!!!!!!!"
+else
+	puts"you can do better"
 
+end
 
-#let's expand out and use the two operators
--- flow_control_loops/loops1.rb
+++ flow_control_loops/loops1.rb
@@ -1,13 +1,13 @@
 counter = 1
 
-until counter == 10
+until counter > 10
 	puts counter
 	counter = counter + 1
 end
 
 while counter < 11
 	puts counter
-	counter = counter + 1
+	counter += 1
 end
 
 for num in 1...10
